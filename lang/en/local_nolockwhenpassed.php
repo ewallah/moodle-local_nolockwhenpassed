824b82005882a54d99ba027dd1a6cca076b1e2c0
eWallah <info@eWallah.net>
2021-03-15 19:49:10 +0100

    GDPR

--- lang/en/local_nolockwhenpassed.php
+++ lang/en/local_nolockwhenpassed.php
@@ -13,4 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 $string['pluginname'] = 'nolockwhenpassed';
+$string['privacy:metadata'] = 'The local nolockwhenpassed plugin does not store private data.';
